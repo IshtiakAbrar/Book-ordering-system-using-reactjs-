f20913f949dc845e97457e0400440ccab28186e0
IshtiakAbrar <ishtiakabrar1200@gmail.com>
2022-04-25 17:24:16 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    "BASE_URL" : "http://localhost:3000",
+    "MONGODB_URL" : "mongodb+srv://ishtiakAbrar:ishtu1200@e-book.wqtjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET" : "shnova0987654321@gmail.com01672019292123412",
+    "REFRESH_TOKEN_SECRET" : "shnova0987654321@gmail.com0167201929201303082197",
+    "CLOUD_UPDATE_PRESET": "nextjs_store",
+    "CLOUD_NAME": "shoaibahamed",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/shoaibahamed/image/upload"
+  }
+}
